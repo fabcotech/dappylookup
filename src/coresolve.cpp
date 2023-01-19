dec4baaaac4eb05a39928b69931fe73c87980459
Fabco <contact@fabco.tech>
2023-01-19 17:17:08 +0100

    Added cargs for proper handling of cli args

---
+++ src/coresolve.cpp
@@ -0,0 +1,137 @@
+// Your First C++ Program
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include <cstring>
+#include "request.cpp"
+#include "network.cpp"
+
+std::string coresolution_result(
+  bool debug,
+  int network_size,
+  std::map<std::string, std::string> responses
+) {
+
+  int responses_size = 0;
+  std::map<std::string, std::string>::iterator it;
+  for(it = responses.begin(); it != responses.end(); ++ it){
+    responses_size += 1;
+  }
+
+  std::map<int, int> absolutes;
+  absolutes[1] = 1;
+  absolutes[2] = 2;
+  absolutes[3] = 2;
+  absolutes[4] = 3;
+  absolutes[5] = 3;
+  absolutes[6] = 4;
+  absolutes[7] = 4;
+
+  std::map<int, int> accuracies;
+  accuracies[1] = 100;
+  accuracies[2] = 100;
+  accuracies[3] = 66;
+  accuracies[4] = 66;
+  accuracies[5] = 66;
+  accuracies[6] = 66;
+  accuracies[7] = 66;
+
+  if (debug) {
+    std::cout << "[should stop] Should we stop co-resolution ?" << std::endl;
+    std::cout << "[should stop] Responses at that time : " << responses_size << "/" << network_size << std::endl;
+    std::cout << "[should stop] Will check for absolute " << absolutes[network_size] << std::endl;
+    std::cout << "[should stop] Will check for accuracy " << accuracies[network_size] << std::endl;
+  }
+
+  if (responses_size >= absolutes[network_size]) {
+    if (debug)
+      std::cout << "[should stop] Absolute reached, co-resolution should be either successful/unsuccessful" << std::endl;
+
+    std::map<std::string, int> counts;
+    std::map<std::string, std::string>::iterator it;
+    for(it = responses.begin(); it != responses.end(); ++ it){
+      if (counts.find(responses[it->first]) == counts.end()) {
+        counts[responses[it->first]] = 1;
+      } else {
+        counts[responses[it->first]] += 1;
+      }
+    }
+
+    std::map<std::string, int>::iterator it2;
+    for(it2 = counts.begin(); it2 != counts.end(); ++ it2){
+      float f = it2->second;
+      float percent = 100 * (f / responses_size);
+      if (debug)
+        std::cout << "[should stop] checking for " << (it2->first).substr(0,30) << " => " << it2->second << " -- " << percent << "%" << std::endl;
+
+      if (percent >= accuracies[network_size]) {
+        if (debug) {
+          std::cout << "[should stop] Found a response over accuracies threshold" << std::endl;
+          std::cout << it2->first << std::endl;
+        }
+        return it2->first;
+      }
+    }
+  } else {
+    if (debug)
+      std::cout << "Absolute not reached, co-resolution must continue" << std::endl;
+  }
+
+  return "continue";
+}
+
+int coresolve(
+  bool debug,
+  std::string record_type
+) {
+  if (debug)
+    std::cout << "starting co-resolution" << std::endl;
+
+  std::vector<std::map<std::string, std::string>> network = getNetwork("d");
+  int network_size = network.size();
+
+  if (debug)
+    std::cout << network_size << " members" << std::endl;
+
+  std::map<std::string, std::string> responses;
+
+  std::string final_result = "ERR_COULD_NOT_RESOLVE";
+  //const std::map<std::string, std::string> responses = {};
+  for (int i = 0; i < network_size; i++) {
+
+    if (debug)
+      std::cout << "[request no" << i << "]" << std::endl;
+
+    std::cout << network[i].at("hostname") << std::endl;
+    std::string response;
+    try {
+      response = request(network[i], debug);
+      std::cout << "----- request success" << std::endl;
+      std::cout << response.substr(0,40) << std::endl;
+      responses[network[i].at("hostname")] = response;
+    } catch (std::string err){
+      std::cout << "----- request error" << std::endl;
+      std::cout << err.substr(0,40) << std::endl;
+    }
+
+    std::string a = "[request no" + std::to_string(i) + "]";
+    if (debug) {
+      std::cout << a << " successful" << std::endl;
+      std::cout << a << " status code 200" << std::endl;
+      std::cout << response.substr(0, 20) << std::endl;
+    }
+
+    std::string should_stop = coresolution_result(debug, network_size, responses);
+    if (should_stop != "continue") {
+      final_result = should_stop;
+      break;
+    }
+  }
+
+  std::cout << "Final result of co-resolution : " << std::endl;
+  std::cout << final_result << std::endl;
+
+  return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -5,138 +5,115 @@
 #include <vector>
 #include <string>
 #include <cstring>
-#include "request.cpp"
-#include "network.cpp"
+#include <cargs.h>
+#include "coresolve.cpp"
 
 bool debug = false;
+bool help = false;
+bool version = false;
 
-std::string coresolution_result(
-  int network_size,
-  std::map<std::string, std::string> responses
-) {
+static struct cag_option options[] = {
+  {.identifier = 'd',
+   .access_letters = "d",
+   .access_name = "debug",
+   .value_name = NULL,
+   .description = "Run in debug mode with many logs for co-resolution and TLS session"},
 
-  int responses_size = 0;
-  std::map<std::string, std::string>::iterator it;
-  for(it = responses.begin(); it != responses.end(); ++ it){
-    responses_size += 1;
-  }
+  {.identifier = 'v',
+   .access_letters = "v",
+   .access_name = "version",
+   .value_name = NULL,
+   .description = "Simple flag"},
 
-  std::map<int, int> absolutes;
-  absolutes[1] = 1;
-  absolutes[2] = 2;
-  absolutes[3] = 2;
-  absolutes[4] = 3;
-  absolutes[5] = 3;
-  absolutes[6] = 4;
-  absolutes[7] = 4;
+  {.identifier = 't',
+    .access_letters = "t",
+    .access_name = "type",
+    .value_name = "TYPE",
+    .description = "Type of record A / AAAA / TXT / CERT / CNAME"},
 
-  std::map<int, int> accuracies;
-  accuracies[1] = 100;
-  accuracies[2] = 100;
-  accuracies[3] = 66;
-  accuracies[4] = 66;
-  accuracies[5] = 66;
-  accuracies[6] = 66;
-  accuracies[7] = 66;
+  {.identifier = 'h',
+    .access_letters = "h",
+    .access_name = "help",
+    .description = "Shows the command help"}};
 
-  if (debug) {
-    std::cout << "[should stop] Should we stop co-resolution ?" << std::endl;
-    std::cout << "[should stop] Responses at that time : " << responses_size << "/" << network_size << std::endl;
-    std::cout << "[should stop] Will check for absolute " << absolutes[network_size] << std::endl;
-    std::cout << "[should stop] Will check for accuracy " << accuracies[network_size] << std::endl;
-  }
-
-  if (responses_size >= absolutes[network_size]) {
-    if (debug)
-      std::cout << "[should stop] Absolute reached, co-resolution should be either successful/unsuccessful" << std::endl;
-
-    std::map<std::string, int> counts;
-    std::map<std::string, std::string>::iterator it;
-    for(it = responses.begin(); it != responses.end(); ++ it){
-      if (counts.find(responses[it->first]) == counts.end()) {
-        counts[responses[it->first]] = 1;
-      } else {
-        counts[responses[it->first]] += 1;
-      }
-    }
-
-    std::map<std::string, int>::iterator it2;
-    for(it2 = counts.begin(); it2 != counts.end(); ++ it2){
-      float f = it2->second;
-      float percent = 100 * (f / responses_size);
-      if (debug)
-        std::cout << "[should stop] checking for " << (it2->first).substr(0,30) << " => " << it2->second << " -- " << percent << "%" << std::endl;
-
-      if (percent >= accuracies[network_size]) {
-        if (debug) {
-          std::cout << "[should stop] Found a response over accuracies threshold" << std::endl;
-          std::cout << it2->first << std::endl;
-        }
-        return it2->first;
-      }
-    }
-  } else {
-    if (debug)
-      std::cout << "Absolute not reached, co-resolution must continue" << std::endl;
-  }
-
-  return "continue";
-}
+struct demo_configuration
+{
+  bool simple_flag;
+  bool multiple_flag;
+  bool long_flag;
+  const char *key;
+};
 
 int main(int argc, char** argv) {
-  for (int i = 0; i < argc; ++i) {
-    if (strcmp(argv[i], "--debug") == 0) {
-      std::cout << "[debug mode]" << std::endl;
+  char identifier;
+  const char *value;
+  cag_option_context context;
+  struct demo_configuration config = {false, false, false, NULL};
+  int param_index;
+
+  /**
+   * Now we just prepare the context and iterate over all options. Simple!
+   */
+  cag_option_prepare(&context, options, CAG_ARRAY_SIZE(options), argc, argv);
+  while (cag_option_fetch(&context)) {
+    identifier = cag_option_get(&context);
+    switch (identifier) {
+    case 'd':
       debug = true;
-    }
-  }
-
-  if (debug)
-    std::cout << "starting co-resolution" << std::endl;
-
-  std::vector<std::map<std::string, std::string>> network = getNetwork("d");
-  int network_size = network.size();
-
-  if (debug)
-    std::cout << network_size << " members" << std::endl;
-
-  std::map<std::string, std::string> responses;
-
-  std::string final_result = "ERR_COULD_NOT_RESOLVE";
-  //const std::map<std::string, std::string> responses = {};
-  for (int i = 0; i < network_size; i++) {
-
-    if (debug)
-      std::cout << "[request no" << i << "]" << std::endl;
-
-    std::cout << network[i].at("hostname") << std::endl;
-    std::string response;
-    try {
-      response = request(network[i], debug);
-      std::cout << "----- request success" << std::endl;
-      std::cout << response.substr(0,40) << std::endl;
-      responses[network[i].at("hostname")] = response;
-    } catch (std::string err){
-      std::cout << "----- request error" << std::endl;
-      std::cout << err.substr(0,40) << std::endl;
-    }
-
-    std::string a = "[request no" + std::to_string(i) + "]";
-    if (debug) {
-      std::cout << a << " successful" << std::endl;
-      std::cout << a << " status code 200" << std::endl;
-      std::cout << response.substr(0, 20) << std::endl;
-    }
-
-    std::string should_stop = coresolution_result(network_size, responses);
-    if (should_stop != "continue") {
-      final_result = should_stop;
+      config.simple_flag = true;
       break;
+    case 'v':
+      version = true;
+      config.simple_flag = true;
+      break;
+    case 'm':
+      config.multiple_flag = true;
+      break;
+    case 'l':
+      config.long_flag = true;
+      break;
+    case 't':
+      std::cout << "yes TYPE";
+      value = cag_option_get_value(&context);
+      config.key = value;
+      break;
+    case 'h':
+      printf("Usage: cargsdemo [OPTION]...\n");
+      printf("Demonstrates the cargs library.\n\n");
+      cag_option_print(options, CAG_ARRAY_SIZE(options), stdout);
+      printf("\nNote that all formatting is done by cargs.\n");
+      return EXIT_SUCCESS;
     }
   }
 
-  std::cout << "Final result of co-resolution : " << std::endl;
-  std::cout << final_result << std::endl;
+  printf("simple_flag: %i, multiple_flag: %i, long_flag: %i, key: %s\n",
+    config.simple_flag, config.multiple_flag, config.long_flag,
+    config.key ? config.key : "-");
 
-  return 0;
+  for (param_index = context.index; param_index < argc; ++param_index) {
+    printf("additional parameter: %s\n", argv[param_index]);
+  }
+
+  if (version) {
+    std::cout << "version" << std::endl;
+    return 0;
+  }
+
+  std::string record_type = "A";
+  if (config.key) {
+    if (
+      config.key == std::string("A") ||
+      config.key == std::string("AAAA") ||
+      config.key == std::string("TXT") ||
+      config.key == std::string("CERT") ||
+      config.key == std::string("CNAME")
+    ) {
+      record_type = config.key;
+    } else {
+      std::cout << std::string("unrecognized --type parameter") << std::endl;
+      return 1;
+    }
+  }
+
+  return coresolve(debug, record_type);
 }
